bc4a401e6d04a4c3d35dcfb13ca490741515deb5
davidovits1 <david.o3111@gmail.com>
2023-04-30 23:50:43 +0300

    I added main and changed variable names

--- 6.3_a_long_cat_is_long.py
+++ 6.3_a_long_cat_is_long.py
@@ -1,3 +1,13 @@
+
+MY_TEXT = """
+You see, wire telegraph is a kind of a very, very long cat.
+You pull his tail in New York and his head is meowing in Los Angeles.
+Do you understand this?
+And radio operates exactly the same way: you send signals here, they receive them there.
+The only difference is that there is no cat.
+"""
+
+
 def count_words(text):
     """
     The function accepts text and returns a dictionary where the key is a word and the value is the length of the word.
@@ -6,17 +16,16 @@ def count_words(text):
     :param text: text
     :return: dictionary_text
     """
-    temp_text = (w for w in text if w.isalpha() or w == " ")  # generator expression
-    new_text = "".join((list(temp_text)))
+    generator_text = (char for char in text if char.isalpha() or char == " ")  # generator expression
+    new_text = "".join((list(generator_text)))
     dictionary_text = {word: len(word) for word in new_text.split(' ') if word != ""}  # dictionary comprehension
     return dictionary_text
 
 
-my_text = """
-You see, wire telegraph is a kind of a very, very long cat.
-You pull his tail in New York and his head is meowing in Los Angeles.
-Do you understand this?
-And radio operates exactly the same way: you send signals here, they receive them there.
-The only difference is that there is no cat.
-"""
-print(count_words(my_text))
+def main():
+    print(count_words(MY_TEXT))
+
+
+if __name__ == "__main__":
+    # Call the main handler function
+    main()
