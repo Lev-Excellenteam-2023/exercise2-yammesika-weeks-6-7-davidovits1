ec38883bc3334b3b6aae63d2f40baa9ef2f3617e
davidovits1 <david.o3111@gmail.com>
2023-04-30 22:23:38 +0300

    I added main and renamed the variables

--- 6.5_group_by.py
+++ 6.5_group_by.py
@@ -1,20 +1,27 @@
-def group_by(f, iterable):
+
+def group_by(func, iterable):
     """
     get a function and an iterable and returns a dictionary whose keys are
      the activation of the function on each member and the values are the members
-    :param f: function for key
+    :param func: function for key
     :param iterable: iterable
     :return: group dictionary by function
     """
     group_dictionary = {}
-    for i in iterable:
-        if f(i) in group_dictionary:
-            group_dictionary[f(i)].append(i)
+    for item in iterable:
+        if func(item) in group_dictionary:
+            group_dictionary[func(item)].append(item)
         else:
-            group_dictionary[f(i)] = [i]
+            group_dictionary[func(item)] = [item]
 
     return group_dictionary
 
 
-assert group_by(len, ["hi", "bye", "yo", "try"]) == {2: ['hi', 'yo'], 3: ['bye', 'try']}, "Not the expected result"
-assert group_by(len, ["", "", "", ""]) == {0: ['', '', '', '']}, "Not the expected result"
+def main():
+    assert group_by(len, ["hi", "bye", "yo", "try"]) == {2: ['hi', 'yo'], 3: ['bye', 'try']}, "Not the expected result"
+    assert group_by(len, ["", "", "", ""]) == {0: ['', '', '', '']}, "Not the expected result"
+
+
+if __name__ == "__main__":
+    # Call the main handler function
+    main()
