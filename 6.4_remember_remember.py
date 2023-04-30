e38d0df389c43e380611c3136ba902ae822e817f
davidovits1 <david.o3111@gmail.com>
2023-04-30 23:24:07 +0300

    I added main, changed the names of variables, changed the code structure (adding a function) and shortened the code

--- 6.4_remember_remember.py
+++ 6.4_remember_remember.py
@@ -1,3 +1,4 @@
+
 from PIL import Image
 import os.path
 
@@ -12,21 +13,33 @@ def decryption(path):
     """
     if os.path.exists(path):
         try:
-            im = Image.open(path)
+            image = Image.open(path)
         except IOError:
             return "the path is not an image file"
-        pix = im.load()
-        width, hight = im.size
-        code = ""
-        for i in range(width):
-            for j in range(hight):
-                if pix[i, j] == 1:
-                    code += chr(j)
+        code = code_from_image(image)
+
     else:
         code = "the path not exists"
     return code
 
 
-assert decryption("resources//hamlet.txt") == "the path is not an image file", "Not the expected result"
-assert decryption("blabla.com") == "the path not exists", "Not the expected result"
-print("decryption of the cipher: " + decryption(MY_PATH))
+def code_from_image(image):
+    """
+    Decodes a code hidden in pixels of image
+    :param image: image object
+    :return: the code hidden in the image
+    """
+    pix = image.load()
+    width, height = image.size
+    return ''.join(chr(column) for row in range(width) for column in range(height) if pix[row, column] == 1)
+
+
+def main():
+    assert decryption("resources//hamlet.txt") == "the path is not an image file", "Not the expected result"
+    assert decryption("blabla.com") == "the path not exists", "Not the expected result"
+    print("decryption of the cipher: " + decryption(MY_PATH))
+
+
+if __name__ == "__main__":
+    # Call the main handler function
+    main()
